02cb4598d53e4acabb3184c965b9108c761ace5b
bulbashist <nbulbashist@gmail.com>
2023-03-09 15:24:26 +0300

    lab3

--- index.ts
+++ index.ts
@@ -1,111 +1,109 @@
 import { ConfigData, Point } from "./types.js";
 
-// классифицируем объект -> с учетом априорной считаем по байесу
+function tt(ptCount: number, p1: number, p2: number): void {
+  const configData = new ConfigData(ptCount, p1, p2);
 
-// событие - Ck - объект относится к классу K
-// причина -
+  document.querySelector(".info")!.innerHTML = "";
 
-const configData = new ConfigData(100, 0.4, 0.6);
+  const calculateExpVal = (j: number) => {
+    const data = configData.data[j];
+    return (
+      data.points.reduce((acc, curr) => (acc += curr), 0) / data.points.length
+    );
+  };
 
-// Математическое ожидание
-const calculateExpVal = (j: number) => {
-  const data = configData.data[j];
-  return (
-    data.points.reduce((acc, curr) => (acc += curr), 0) / data.points.length
-  );
-};
+  const expVals = [] as number[];
+  expVals[0] = calculateExpVal(0);
+  expVals[1] = calculateExpVal(1);
 
-const expVals = [] as number[];
-expVals[0] = calculateExpVal(0);
-expVals[1] = calculateExpVal(1);
+  const calculateStdDev = (j: number) => {
+    const data = configData.data[j];
+    return Math.sqrt(
+      data.points.reduce(
+        (acc, curr) => (acc += Math.pow(curr - expVals[j], 2)),
+        0
+      ) / data.points.length
+    );
+  };
 
-// СКО
-const calculateStdDev = (j: number) => {
-  const data = configData.data[j];
-  return Math.sqrt(
-    data.points.reduce(
-      (acc, curr) => (acc += Math.pow(curr - expVals[j], 2)),
-      0
-    ) / data.points.length
-  );
-};
+  const stdDevs = [] as number[];
+  stdDevs[0] = calculateStdDev(0);
+  stdDevs[1] = calculateStdDev(1);
 
-const stdDevs = [] as number[];
-stdDevs[0] = calculateStdDev(0);
-stdDevs[1] = calculateStdDev(1);
+  let Pxk = (x: number, j: number) => {
+    return (
+      (1 / (stdDevs[j] * Math.sqrt(2 * Math.PI))) *
+      Math.pow(Math.E, (-1 / 2) * Math.pow((x - expVals[j]) / stdDevs[j], 2))
+    );
+  };
 
-let Pxk = (x: number, j: number) => {
-  return (
-    (1 / (stdDevs[j] * Math.sqrt(2 * Math.PI))) *
-    Math.pow(Math.E, (-1 / 2) * Math.pow((x - expVals[j]) / stdDevs[j], 2))
-  );
-};
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  canvas.width = 1000;
+  canvas.height = 400;
+  const ctx = canvas.getContext("2d")!;
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-canvas.width = 1000;
-canvas.height = 600;
-const ctx = canvas.getContext("2d")!;
-
-ctx.fillStyle = "rgb(0, 255, 0)";
-for (let i = 0; i < 1000; i++) {
-  const y = configData.data[0].probability * Pxk(i, 0) * 100000;
-  ctx.fillRect(i, 600 - y + 1, 1, 1);
-}
-
-ctx.fillStyle = "rgb(255, 0, 0)";
-for (let i = 0; i < 1000; i++) {
-  const y = configData.data[1].probability * Pxk(i, 1) * 100000;
-  ctx.fillRect(i, 600 - y + 1, 1, 1);
-}
-
-let zlt1 = 0;
-let zpo1 = 0;
-for (let i = 0; i < 1000; i++) {
-  const t1 = configData.data[0].probability * Pxk(i, 0);
-  const t2 = configData.data[1].probability * Pxk(i, 1);
-
-  if (t1 > t2) {
-    zlt1 += t2;
+  ctx.fillStyle = "rgb(0, 255, 0)";
+  for (let i = 0; i < 1000; i++) {
+    const y1 = configData.data[0].probability * Pxk(i, 0) * 1000 * 100;
+    ctx.fillRect(i, canvas.height - y1 + 1, 1, 1);
   }
 
-  if (t2 > t1) {
-    zpo1 += t1;
-  }
-}
-
-let zlt2 = 0;
-let zpo2 = 0;
-for (let i = 0; i < 1000; i++) {
-  const t1 = configData.data[0].probability * Pxk(i, 0);
-  const t2 = configData.data[1].probability * Pxk(i, 1);
-
-  if (t2 > t1) {
-    zlt2 += t1;
+  ctx.fillStyle = "rgb(255, 0, 0)";
+  for (let i = 0; i < 1000; i++) {
+    const y2 = configData.data[1].probability * Pxk(i, 1) * 1000 * 100;
+    ctx.fillRect(i, canvas.height - y2 + 1, 1, 1);
   }
 
-  if (t1 > t2) {
-    zpo2 += t2;
-  }
-}
+  let zlt1 = 0;
+  let zpo1 = 0;
+  let zlt2 = 0;
+  let zpo2 = 0;
+  for (let i = 0; i < 1000; i++) {
+    const t1 = configData.data[0].probability * Pxk(i, 0);
+    const t2 = configData.data[1].probability * Pxk(i, 1);
 
-const dataC1 = `
+    if (t1 > t2) {
+      zlt1 += t2;
+      zpo2 += t2;
+    }
+
+    if (t2 > t1) {
+      zpo1 += t1;
+      zlt2 += t1;
+    }
+  }
+
+  const dataC1 = `
   <h2>Для класса 1:</h2>
   <p>Зона ложной тревоги: ${zlt1}</p>
   <p>Зона пропуска обнаружения: ${zpo1}</p>
   <p>Суммарная ошибка классификации: ${zlt1 + zpo1}</p>
 `;
 
-const dataBlockC1 = document.createElement("div");
-dataBlockC1.innerHTML = dataC1;
-document.body.append(dataBlockC1);
+  const dataBlockC1 = document.createElement("div");
+  dataBlockC1.innerHTML = dataC1;
+  document.querySelector(".info")!.append(dataBlockC1);
 
-const dataC2 = `
-  <h2>Для класса 2:</h2>
-  <p>Зона ложной тревоги: ${zlt2}</p>
-  <p>Зона пропуска обнаружения: ${zpo2}</p>
-  <p>Суммарная ошибка классификации: ${zlt2 + zpo2}</p>
-`;
+  //   const dataC2 = `
+  //   <h2>Для класса 2:</h2>
+  //   <p>Зона ложной тревоги: ${zlt2}</p>
+  //   <p>Зона пропуска обнаружения: ${zpo2}</p>
+  //   <p>Суммарная ошибка классификации: ${zlt2 + zpo2}</p>
+  // `;
 
-const dataBlockC2 = document.createElement("div");
-dataBlockC2.innerHTML = dataC2;
-document.body.append(dataBlockC2);
+  //   const dataBlockC2 = document.createElement("div");
+  //   dataBlockC2.innerHTML = dataC2;
+  //   document.querySelector(".info")!.append(dataBlockC2);
+}
+
+tt(100, 0.3, 0.7);
+
+document.querySelector("#btn")!.addEventListener("click", () => {
+  const vals = [] as number[];
+  const inputs = document.querySelectorAll(".input")!;
+
+  inputs.forEach((input, index) => {
+    vals[index] = Number((input as HTMLInputElement).value);
+  });
+  tt(10, vals[0], vals[1]);
+});
--- types.ts
+++ types.ts
@@ -1,3 +1,49 @@
+function randn_bm(): number {
+  let u = 0,
+    v = 0;
+  while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+  while (v === 0) v = Math.random();
+  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+  num = num / 10.0 + 0.5; // Translate to 0 -> 1
+  if (num > 1 || num < 0) return randn_bm(); // resample between 0 and 1
+  return num;
+}
+
+function randomIntegerInRange(min: number, max: number) {
+  return Math.floor(randn_bm() * (max - min + 1)) + min;
+}
+
+console.log(randomIntegerInRange(1, 6));
+
+class Gauss {
+  ready = false;
+  second = 0.0;
+
+  next(mean: number, dev: number) {
+    mean = mean == undefined ? 0.0 : mean;
+    dev = dev == undefined ? 1.0 : dev;
+
+    if (this.ready) {
+      this.ready = false;
+      return this.second * dev + mean;
+    } else {
+      var u, v, s;
+      do {
+        u = 2.0 * Math.random() - 1.0;
+        v = 2.0 * Math.random() - 1.0;
+        s = u * u + v * v;
+      } while (s > 1.0 || s == 0.0);
+
+      var r = Math.sqrt((-2.0 * Math.log(s)) / s);
+      this.second = r * u;
+      this.ready = true;
+      return r * v * dev + mean;
+    }
+  }
+}
+
+const g = new Gauss(); // создаём объект
+
 export class ConfigData {
   public data: ClusterData[] = [new ClusterData(), new ClusterData()];
 
@@ -7,11 +53,14 @@ export class ConfigData {
 
     // ну не Гаусс ну и ладно
     for (let i = 0; i < n; i++) {
-      this.data[0].points.push(Math.random() * 700);
+      // this.data[0].points.push(Math.random() * 700);
+      this.data[0].points.push(g.next(300, 150));
+      this.data[1].points.push(g.next(700, 150));
     }
+    console.log(g.next(700, 1));
 
     for (let i = 0; i < n; i++) {
-      this.data[1].points.push(Math.random() * 700 + 300);
+      // this.data[1].points.push(Math.random() * 700 + 300);
     }
   }
 }
